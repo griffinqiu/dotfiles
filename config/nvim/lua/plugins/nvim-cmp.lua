a24f3e608c000545f170d7de9e2bee52c0d34277
Griffin Qiu <griffinqiu@gmail.com>
2023-08-04 12:17:22 +0800

    Fixed the git

--- config/nvim/lua/plugins/nvim-cmp.lua
+++ config/nvim/lua/plugins/nvim-cmp.lua
@@ -126,6 +126,26 @@ return {
       -- })
     -- })
 
+    _G.vimrc = _G.vimrc or {}
+    _G.vimrc.cmp = _G.vimrc.cmp or {}
+    _G.vimrc.cmp.lsp = function()
+      cmp.complete({
+        config = {
+          sources = {
+            { name = 'nvim_lsp' }
+          }
+        }
+      })
+    end
+    _G.vimrc.cmp.snippet = function()
+      cmp.complete({
+        config = {
+          sources = {
+            { name = 'vsnip' }
+          }
+        }
+      })
+    end
     vim.cmd([[
       inoremap <C-x><C-o> <Cmd>lua vimrc.cmp.lsp()<CR>
       inoremap <C-x><C-s> <Cmd>lua vimrc.cmp.snippet()<CR>
--- config/nvim/lua/plugins/scm.lua
+++ config/nvim/lua/plugins/scm.lua
@@ -1,9 +1,10 @@
 return {
   {
     'mhinz/vim-signify',
-    config = function()
+    init = function()
       vim.g.signify_vcs_list = { 'git' }
     end,
+    lazy = false,
     keys = {
       { "ic", "<plug>(signify-motion-inner-pending)", mode="o" },
       { "ic", "<plug>(signify-motion-inner-visual)", mode="x" },
@@ -13,16 +14,16 @@ return {
       { "[g", "<plug>(signify-prev-hunk)", mode="n" },
       { "]G", "9999]g", mode="n" },
       { "[G", "9999[g", mode="n" },
-      { "<leader>gt", ":SignifyToggle<CR>", mode="n" },
-      { "<leader>gh", ":SignifyToggleHighlight<CR>", mode="n" },
-      { "<leader>gr", ":SignifyRefresh<CR>", mode="n" },
+      { "<leader>gt", "<cmd>SignifyToggle<CR>", mode="n" },
+      { "<leader>gh", "<cmd>SignifyToggleHighlight<CR>", mode="n" },
+      { "<leader>gr", "<cmd>SignifyRefresh<CR>", mode="n" },
     },
   },
 
   {
     'tpope/vim-fugitive',
     keys = {
-      { "<leader> gb", ":Git blame<br>" }
+      { "<leader>gb", '<cmd>Git blame<cr>', mode="n" }
     },
   },
   { 'tpope/vim-rhubarb' }, -- :Gbrowse
