b21949d6d4b4d52c045f3b7b34e38612b1017af9
Griffin Qiu <griffinqiu@gmail.com>
2026-01-31 15:38:41 +0800

    adjust path

--- zsh/configs/post/path.zsh
+++ zsh/configs/post/path.zsh
@@ -1,5 +1,5 @@
-# ensure dotfiles bin directory is loaded first
-PATH="$HOME/.bin:/usr/local/sbin:$PATH"
+# ensure homebrew and dotfiles bin directory is loaded first
+PATH="$HOME/.bin:/opt/homebrew/bin:/usr/local/sbin:$PATH"
 
 # mkdir .git/safe in the root of repositories you trust
 PATH=".git/safe/../../bin:$PATH"
