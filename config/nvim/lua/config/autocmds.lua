5f2f9e680c68941ce339c46ceca0b973f44eeb10
Griffin Qiu <griffinqiu@gmail.com>
2026-02-03 20:04:26 +0800

    fix(autocmds): remove deprecated autocmd group for spell checking

--- config/nvim/lua/config/autocmds.lua
+++ config/nvim/lua/config/autocmds.lua
@@ -7,6 +7,8 @@
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
 
+pcall(vim.api.nvim_del_augroup_by_name, "lazyvim_wrap_spell")
+
 vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
   pattern = "Dockerfile.builder",
   command = "set filetype=dockerfile",
