39d7144379b7fcd63f02cc8c8f312fcdd451ecf9
Griffin Qiu <griffinqiu@gmail.com>
2026-02-14 12:32:43 +0800

    fix(path): reorder asdf shims in PATH for correct hierarchy

--- zsh/configs/post/path.zsh
+++ zsh/configs/post/path.zsh
@@ -1,7 +1,7 @@
 PATH="/usr/local/sbin:$PATH"
 PATH="$HOME/.bun/bin:$PATH"
-PATH="${ASDF_DATA_DIR:-$HOME/.asdf}/shims:$PATH"
 PATH="/opt/homebrew/bin:$PATH"
+PATH="${ASDF_DATA_DIR:-$HOME/.asdf}/shims:$PATH"
 PATH="$HOME/.bin:$PATH"
 PATH="$HOME/.local/bin:$PATH"
 PATH=".git/safe/../../bin:$PATH"
